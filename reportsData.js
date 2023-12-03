94dbea5f00d4cb1f03e01a4d0494c6c168e1d7ca
Aakanksha Pharande <aka.pharande@gmail.com>
2023-12-03 16:07:52 -0500

    Added a list of reports from sample data

---
+++ reportsData.js
@@ -0,0 +1,21 @@
+const sampleReports = [
+    {
+      id: 1,
+      testName: 'Air Quality Test',
+      clientId: 2,
+      clientName: 'ABC Company',
+      date: '2023-12-01',
+      result: 'Pass',
+    },
+    {
+      id: 2,
+      testName: 'Water Quality Test',
+      clientId: 1,
+      clientName: 'XYZ Corporation',
+      date: '2023-12-02',
+      result: 'Fail',
+    },
+  ];
+  
+  module.exports = sampleReports;
+  
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
-var express = require('express');
-var router = express.Router();
+// routes/index.js
+const express = require('express');
+const router = express.Router();
+const sampleReports = require('../reportsData');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Environmental Testing Lab' });
+});
+
+// Add a new route to fetch and display sample reports
+router.get('/reports', function (req, res, next) {
+  res.render('reports', { title: 'Test Reports', reports: sampleReports });
 });
 
 module.exports = router;
